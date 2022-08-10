0d1d9603d674d44fa987545f1c1b0471aac61f0f
LODNight <khuunhattin123@gmail.com>
2022-08-10 18:34:51 +0700

    a

--- resources/views/block/head_css_script.blade.php
+++ resources/views/block/head_css_script.blade.php
@@ -1,41 +1,45 @@
-<meta charset="UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>Your Store</title>
-<base  />
-<meta name="description" content="My Store" />
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/jquery/jquery-2.1.1.min.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/bootstrap/js/bootstrap.min.js"></script>
-<link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
-<link href='https://fonts.googleapis.com/css?family=Muli:300,400,500,600,400,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/jquery/owl-carousel/owl.carousel.min.css" rel="stylesheet" media="screen" />
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/jquery/owl-carousel/owl.theme.default.min.css" rel="stylesheet" media="screen" />
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/theme/OPC164_01/stylesheet/TemplateTrip/bootstrap.min.css" rel="stylesheet" media="screen" />
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/theme/OPC164_01/stylesheet/stylesheet.css" rel="stylesheet">
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/theme/OPC164_01/stylesheet/TemplateTrip/ttblogstyle.css" rel="stylesheet" type="text/css" />
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/theme/OPC164_01/stylesheet/TemplateTrip/newsletter.css" rel="stylesheet" type="text/css" />
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/theme/OPC164_01/stylesheet/TemplateTrip/animate.css" rel="stylesheet" type="text/css" />
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/theme/OPC164_01/stylesheet/TemplateTrip/ttcountdown.css" rel="stylesheet" type="text/css" />
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/theme/OPC164_01/stylesheet/TemplateTrip/menu.css" rel="stylesheet" type="text/css" />
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/theme/OPC164_01/stylesheet/TemplateTrip/lightbox.css" rel="stylesheet" type="text/css" />
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/jquery/swiper/css/swiper.min.css" type="text/css" rel="stylesheet" media="screen" />
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/jquery/swiper/css/opencart.css" type="text/css" rel="stylesheet" media="screen" />
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/common.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/addonScript.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/tt_quickview.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/inview.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/parallex.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/theia-sticky-sidebar.min.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/menu.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/ResizeSensor.min.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/lightbox-2.6.min.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/waypoints.min.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/bootstrap-notify.min.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/ttcountdown.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/backgroundVideo.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/jquery/owl-carousel/owl.carousel.min.js"></script>
-<link href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/image/catalog/cart.png" rel="icon" />
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/jquery/swiper/js/swiper.jquery.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/jquery.bpopup.min.js"></script>
-<script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/TemplateTrip/jquery.cookie.js"></script>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+         <head>
+            <meta charset="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title>CTTL Clothes</title>
+            <base  />
+            <meta name="description" content="My Store" />
+            <script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/jquery/jquery-2.1.1.min.js"></script>
+            <script src="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/catalog/view/javascript/bootstrap/js/bootstrap.min.js"></script>
+            <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
+            <link href='https://fonts.googleapis.com/css?family=Muli:300,400,500,600,400,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+            <link href="{{asset('css/css1.css')}}" rel="stylesheet" media="screen" />
+            <link href="{{asset('css/css2.css')}}" rel="stylesheet" media="screen" />
+            <link href="{{asset('css/css3.css')}}" rel="stylesheet" media="screen" />
+            <link href="{{asset('css/css4.css')}}" rel="stylesheet">
+            <link href="{{asset('css/css5.css')}}" rel="stylesheet" type="text/css" />
+            <link href="{{asset('css/css6.css')}}" rel="stylesheet" type="text/css" />
+            <link href="{{asset('css/css7.css')}}" rel="stylesheet" type="text/css" />
+            <link href="{{asset('css/css8.css')}}" rel="stylesheet" type="text/css" />
+            <link href="{{asset('css/css9.css')}}" rel="stylesheet" type="text/css" />
+            <link href="{{asset('css/css10.css')}}" rel="stylesheet" type="text/css" />
+            <link href="{{asset('css/css11.css')}}" type="text/css" rel="stylesheet" media="screen" />
+            <link href="{{asset('css/css12.css')}}" type="text/css" rel="stylesheet" media="screen" />
+            
+            <script src="{{asset('javascript/script1.js')}}"></script>
+            <script src="{{asset('javascript/script2.js')}}"></script>
+            <script src="{{asset('javascript/script3.js')}}"></script>
+            <script src="{{asset('javascript/script4.js')}}"></script>
+            <script src="{{asset('javascript/script5.js')}}"></script>
+            <script src="{{asset('javascript/script6.js')}}"></script>
+            <script src="{{asset('javascript/script7.js')}}"></script>
+            <script src="{{asset('javascript/script8.js')}}"></script>
+            <script src="{{asset('javascript/script9.js')}}"></script>
+            <script src="{{asset('javascript/script10.js')}}"></script>
+            <script src="{{asset('javascript/script11.js')}}"></script>
+            <script src="{{asset('javascript/script12.js')}}"></script>
+            <script src="{{asset('javascript/script13.js')}}"></script>
+            <script src="{{asset('javascript/script14.js')}}"></script>
+            <script src="{{asset('javascript/script15.js')}}"></script>
+            <script src="{{asset('javascript/script16.js')}}"></script>
+            <script src="{{asset('javascript/script17.js')}}"></script>
+            <link href="{{asset('images/logo.jpg')}}" rel="stylesheet" />
