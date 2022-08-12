19a5c85f899da69177d6b7131f1f9af02a0eee2d
LODNight <khuunhattin123@gmail.com>
2022-08-12 15:12:26 +0700

    done block

---
+++ resources/views/block/head-extra.blade.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Carrier Bag</title>
+    <base href="https://opencart.templatetrip.com/OPC06/OPC164_peter/OPC01/" />
+    
+    @include('block.script_css')
